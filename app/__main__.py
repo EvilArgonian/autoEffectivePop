e4ec0d51ff1a343f7aa2e9526c40183b1c171ac5
EvilArgonian <pmele@carolina.rr.com>
2022-08-29 00:15:39 -0400

    Create starter setup for stand alone executable version

---
+++ app/__main__.py
@@ -0,0 +1,40 @@
+# file app/__main__.py
+
+import pkg_resources
+import argparse
+import subprocess
+
+
+# print(pkg_resources.resource_string('resources', 'myFile.txt')) # Resource reference
+
+def main():
+    # Launch primary shell script with options here!
+
+    # Initialize parser
+    parser = argparse.ArgumentParser(description="Run the autoEffPop program.")
+
+    # Adding input_species argument as
+    parser.add_argument('input_species', metavar='N', nargs='+', help='a list of all species being input')
+
+    # Adding Ploidy argument (defaults to 1; haploid)
+    parser.add_argument("-p", "--Ploidy", type=int, choices=[1, 2], help="Show Ploidy", default=1)
+
+    # Read arguments from command line
+    args = parser.parse_args()
+
+    scriptCall = '../autoEffLaunch.sh ' # Add ploidy functionality here, as arg before input species. Edit autoEffLaunch appropriately!
+    for inSpec in args.input_species:
+        scriptCall += inSpec + " "
+    subprocess.run(['sh', scriptCall])
+
+    print("Find your desired outputs in the final_output folder corresponding to the input species.")
+    print("Alternatively, find the output values quickly in the consolidated_output file corresponding to the run date.")
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    main()
