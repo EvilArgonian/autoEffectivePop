356abc15aeffe6672fcaf789032bf32e06f0e6dc
EvilArgonian <pmele@carolina.rr.com>
2022-09-12 12:02:39 -0400

    Update ResultsGatherer.py

--- ResultsGatherer.py
+++ ResultsGatherer.py
@@ -6,7 +6,7 @@ resultsSuperFolder = "final_output"
 with open("GatheredResults.txt", "a+") as outFile:
     with open("NoResultsFound.txt", "a+") as badOutFile:
         for filename in os.listdir(resultsSuperFolder):
-            speciesFolder = os.path.join(directory, filename)
+            speciesFolder = os.path.join(resultsSuperFolder, filename)
             if os.path.isdir(speciesFolder):
                 resultsFile = os.path.join(speciesFolder, "Results.txt")
                 if os.path.isfile(resultsFile):
