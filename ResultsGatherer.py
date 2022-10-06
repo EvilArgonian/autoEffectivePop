e72dc14edb253f4a649c84cb6482b4fe1e4e5d67
EvilArgonian <pmele@carolina.rr.com>
2022-10-05 22:14:34 -0400

    Add old output check to ResultsGatherer.py

--- ResultsGatherer.py
+++ ResultsGatherer.py
@@ -5,14 +5,18 @@ resultsSuperFolder = "final_output"
 
 with open("GatheredResults.txt", "w+") as outFile:
     with open("NoResultsFound.txt", "w+") as badOutFile:
-        for filename in os.listdir(resultsSuperFolder):
-            speciesFolder = os.path.join(resultsSuperFolder, filename)
-            if os.path.isdir(speciesFolder):
-                resultsFile = os.path.join(speciesFolder, "Results.txt")
-                if os.path.isfile(resultsFile):
-                    print("Found " + resultsFile)
-                    with open(resultsFile, "r") as readFile:
-                        outFile.write(readFile.readlines()[0])
-                else:
-                    print("Did not find " + resultsFile)
-                    badOutFile.write(filename + "\n")
+        with open("OldRunSpecies", "w+") as oldOutFile:
+            for filename in os.listdir(resultsSuperFolder):
+                speciesFolder = os.path.join(resultsSuperFolder, filename)
+                if os.path.isdir(speciesFolder):
+                    resultsFile = os.path.join(speciesFolder, "Results.txt")
+                    if os.path.isfile(resultsFile):
+                        print("Found " + resultsFile)
+                        with open(resultsFile, "r") as readFile:
+                            if readFile.readlines()[0].startswith("Species"):
+                                oldOutFile.write(filename)
+                            else:
+                                outFile.write(readFile.readlines()[0])
+                    else:
+                        print("Did not find " + resultsFile)
+                        badOutFile.write(filename + "\n")
