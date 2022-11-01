5e9b3d3759c12b2f3f5d66cc5dc43c5a45a8898f
EvilArgonian <pmele@carolina.rr.com>
2022-11-01 10:13:37 -0400

    Update getStrainNames.py

--- getStrainNames.py
+++ getStrainNames.py
@@ -8,16 +8,16 @@ for speciesFolder in os.listdir(outputFolder):
     speciesPath = os.path.join(outputFolder, speciesFolder)
     if os.path.isdir(speciesPath):
             with open("speciesStrainTable.txt", "w") as tableFile:
-            with open(speciesPath + "Strains.txt", "w") as strainFile:
-                strainList = []
-                for muscleAlignment in os.listdir(os.path.join(muscleOutFolder, speciesFolder)):
-                    with open(muscleAlignment, "r") as alignFile:
-                        for line in alignFile.readlines():
-                            if line.startswith(">"):
-                                strainName = re.search("\[strain=(.+?)\]", line).group(1)
-                                if strainName not in strainList:
-                                    strainList.append(strainName)
-                for strain in strainList:
-                    strainFile.write(strain + "\n")
-                    tableFile.write(speciesFolder + "\t" + strain)
-                break # Use to test
+                with open(speciesPath + "Strains.txt", "w") as strainFile:
+                    strainList = []
+                    for muscleAlignment in os.listdir(os.path.join(muscleOutFolder, speciesFolder)):
+                        with open(muscleAlignment, "r") as alignFile:
+                            for line in alignFile.readlines():
+                                if line.startswith(">"):
+                                    strainName = re.search("\[strain=(.+?)\]", line).group(1)
+                                    if strainName not in strainList:
+                                        strainList.append(strainName)
+                    for strain in strainList:
+                        strainFile.write(strain + "\n")
+                        tableFile.write(speciesFolder + "\t" + strain)
+                    break # Use to test
