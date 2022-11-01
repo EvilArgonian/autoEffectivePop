919564e903f0d59152101109b60367ddf8c8252f
EvilArgonian <pmele@carolina.rr.com>
2022-11-01 10:21:35 -0400

    Update getStrainNames.py

--- getStrainNames.py
+++ getStrainNames.py
@@ -4,10 +4,10 @@ import re
 outputFolder = "final_output"
 muscleOutFolder = "muscle_output"
 
-for speciesFolder in os.listdir(outputFolder):
-    speciesPath = os.path.join(outputFolder, speciesFolder)
-    if os.path.isdir(speciesPath):
-        with open("speciesStrainTable.txt", "w") as tableFile:
+with open("speciesStrainTable.txt", "w") as tableFile:
+    for speciesFolder in os.listdir(outputFolder):
+        speciesPath = os.path.join(outputFolder, speciesFolder)
+        if os.path.isdir(speciesPath):
             with open(speciesPath + "Strains.txt", "w") as strainFile:
                 strainList = []
                 speciesMuscleFolder = os.path.join(muscleOutFolder, speciesFolder)
@@ -15,9 +15,14 @@ for speciesFolder in os.listdir(outputFolder):
                     with open(os.path.join(speciesMuscleFolder, muscleAlignment), "r") as alignFile:
                         for line in alignFile.readlines():
                             if line.startswith(">"):
-                                strainName = re.search("\[strain=(.+?)\]", line).group(1)
-                                if strainName not in strainList:
-                                    strainList.append(strainName)
+                                existCheck = re.search("\[strain=(.+?)\]", line)
+                                if existCheck:
+                                    strainName = existCheck.group(1)
+                                    if strainName not in strainList:
+                                        strainList.append(strainName)
+                if len(strainList) == 0:
+                    print("No strains found in " + speciesFolder)
+                    continue
                 for strain in strainList:
                     strainFile.write(strain + "\n")
                     tableFile.write(speciesFolder + "\t" + strain + "\n")
