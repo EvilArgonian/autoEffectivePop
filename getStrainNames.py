5fd8d8649f2406a5b8fb3cf725f50e198a5fe40c
EvilArgonian <pmele@carolina.rr.com>
2022-11-01 10:53:15 -0400

    Update getStrainNames.py

--- getStrainNames.py
+++ getStrainNames.py
@@ -11,19 +11,20 @@ with open("speciesStrainTable.txt", "w") as tableFile:
             with open(speciesPath + "Strains.txt", "w") as strainFile:
                 strainList = []
                 speciesMuscleFolder = os.path.join(muscleOutFolder, speciesFolder)
-                for muscleAlignment in os.listdir(speciesMuscleFolder):
-                    with open(os.path.join(speciesMuscleFolder, muscleAlignment), "r") as alignFile:
-                        for line in alignFile.readlines():
-                            if line.startswith(">"):
-                                existCheck = re.search("\[strain=(.+?)\]", line)
-                                if existCheck:
-                                    strainName = existCheck.group(1)
-                                    if strainName not in strainList:
-                                        strainList.append(strainName)
-                if len(strainList) == 0:
-                    print("No strains found in " + speciesFolder)
-                    continue
-                for strain in strainList:
-                    strainFile.write(strain + "\n")
-                    tableFile.write(speciesFolder + "\t" + strain + "\n")
+                if len(os.listdir(speciesMuscleFolder)) > 0:
+                    for muscleAlignment in os.listdir(speciesMuscleFolder):
+                        with open(os.path.join(speciesMuscleFolder, muscleAlignment), "r") as alignFile:
+                            for line in alignFile.readlines():
+                                if line.startswith(">"):
+                                    existCheck = re.search("\[strain=(.+?)\]", line)
+                                    if existCheck:
+                                        strainName = existCheck.group(1)
+                                        if strainName not in strainList:
+                                            strainList.append(strainName)
+                    if len(strainList) == 0:
+                        print("No strains found in " + speciesFolder)
+                        continue
+                    for strain in strainList:
+                        strainFile.write(strain + "\n")
+                        tableFile.write(speciesFolder + "\t" + strain + "\n")
         print("Completed searching " + speciesFolder)
