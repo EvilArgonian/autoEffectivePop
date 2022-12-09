94626ef5c6e118862ec11c187e30187a9925be82
EvilArgonian <pmele@carolina.rr.com>
2022-12-09 11:22:53 -0500

    Update getProcessTimes.py

--- getProcessTimes.py
+++ getProcessTimes.py
@@ -8,13 +8,13 @@ with open("speciesProcessTimeTable.txt", "w") as tableFile:
     for speciesFolder in os.listdir(outputFolder):
         speciesPath = os.path.join(outputFolder, speciesFolder)
         if os.path.isdir(speciesPath):
-            try:
-                with open(speciesPath + "/Process_Time.txt", "r") as timeFile:
-                    timeString = re.search(r"/Runtime: \d+ seconds/", timeFile.readlines()[1])
-                    runtimeSeconds = int(timeString.split()[1])
-                    tableFile.write(speciesFolder + "\t" + runtimeSeconds + "\n")
-                    print("Found runtime for " + speciesFolder + ": " + runtimeSeconds + " seconds.")
-            except Exception:
-                print("Failed to find runtime for " + speciesFolder + ".")
-                continue
+            #try:
+            with open(speciesPath + "/Process_Time.txt", "r") as timeFile:
+                timeString = re.search(r"/Runtime: \d+ seconds/", timeFile.readlines()[1])
+                runtimeSeconds = int(timeString.split()[1])
+                tableFile.write(speciesFolder + "\t" + runtimeSeconds + "\n")
+                print("Found runtime for " + speciesFolder + ": " + runtimeSeconds + " seconds.")
+            #except Exception:
+                #print("Failed to find runtime for " + speciesFolder + ".")
+                #continue
 print("All done!")
