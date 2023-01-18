fdacc414c301b892f5833d5251ef66ab00b9f3bf
EvilArgonian <pmele@carolina.rr.com>
2023-01-18 15:11:02 -0500

    Undo Tweak Arguments

--- manualCalculations.py
+++ manualCalculations.py
@@ -5,7 +5,7 @@ import re
 import datetime
 
 specName = sys.argv[1]
-if len(sys.argv) > 2:
+if len(sys.argv) > 1:
     print("Arg check: " + str(len(sys.argv)))
     ignoreStrain = bool(sys.argv[2])
 else:
