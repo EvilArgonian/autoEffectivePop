2eabc9bec0efeadd9d4edf9b84d9590d08977053
EvilArgonian <pmele@carolina.rr.com>
2023-01-18 15:37:05 -0500

    Update calculationsOnly.sh

--- calculationsOnly.sh
+++ calculationsOnly.sh
@@ -35,7 +35,7 @@ for specFolder in $(find muscle_output/ -mindepth 1 -maxdepth 1 -type d); do
 	mutRate=$(echo $(python getMutationRate.py ${specLabel}))
 	
 	echo watsThetaS: ${watsThetaS} watsThetaN: ${watsThetaN} watsTheta: ${watsTheta} piS: ${piS} piN: ${piN} pi: ${pi} dendropyTheta: ${dendropyTheta} dendropyPi: ${dendropyPi} mutRate: ${mutRate} specLabel: ${specLabel} strains: "N/A"
-	python calcEffPopSize.py ${watsThetaS} ${watsThetaN} ${watsTheta} ${piS} ${piN} ${pi} ${dendropyTheta} ${dendropyPi} ${mutRate} ${specLabel} ${strains}
+	python calcEffPopSize.py ${watsThetaS} ${watsThetaN} ${watsTheta} ${piS} ${piN} ${pi} ${dendropyTheta} ${dendropyPi} ${mutRate} ${specLabel}
 	
 done
 
