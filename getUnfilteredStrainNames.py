7ebaa14b629b01fab5cbea712115d5fed43127c9
EvilArgonian <pmele@carolina.rr.com>
2023-04-02 16:43:15 -0400

    Update getUnfilteredStrainNames.py

--- getUnfilteredStrainNames.py
+++ getUnfilteredStrainNames.py
@@ -21,6 +21,12 @@ with open("speciesUnfilteredStrainTable.txt", "w") as tableFile:
                     for strain in strainList:
                         strainFile.write(strain + "\n")
                         tableFile.write(speciesFolder + "\t" + strain + "\n")
+                    if len(strainList) > 400:
+                        with open("Over400.txt", "a+") as greater400:
+                            greater400.write(speciesFolder + str(len(strainList)))
+                    else:
+                        with open("Under400.txt", "a+") as lessEq400:
+                            lessEq400.write(speciesFolder + str(len(strainList)))
                 except Exception:
                     continue
         print("Completed searching " + speciesFolder)
