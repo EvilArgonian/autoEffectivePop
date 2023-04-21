86587e4f89c561151f121989efa9579392f2c86c
EvilArgonian <pmele@carolina.rr.com>
2023-04-21 02:16:15 -0400

    Update moveMuscleOutFolders.py

--- moveMuscleOutFolders.py
+++ moveMuscleOutFolders.py
@@ -3,5 +3,6 @@ import shutil
 
 for folder in os.listdir("muscle_output/"):
     shutil.copytree(os.path.join("muscle_output", folder), os.path.join("temp", folder, "muscle_output"))
+    print(folder)
     break
 
