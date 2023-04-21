d215e150008760007aaef961c2e85dff044ea8e5
EvilArgonian <pmele@carolina.rr.com>
2023-04-21 02:17:34 -0400

    Update moveMuscleOutFolders.py

--- moveMuscleOutFolders.py
+++ moveMuscleOutFolders.py
@@ -2,7 +2,8 @@ import os
 import shutil
 
 for folder in os.listdir("muscle_output/"):
-    shutil.copytree(os.path.join("muscle_output", folder), os.path.join("temp", folder, "muscle_output"))
-    print(folder)
-    break
+    try:
+        shutil.copytree(os.path.join("muscle_output", folder), os.path.join("temp", folder, "muscle_output"))
+    except Exception:
+        print("Move failed for " + str(folder))
 
