78ca5640703d029da7c6c5a80120cd87c8a66c10
EvilArgonian <pmele@carolina.rr.com>
2023-04-29 15:22:55 -0400

    Update splitConsensus.py

--- Core_Gene_Finder/splitConsensus.py
+++ Core_Gene_Finder/splitConsensus.py
@@ -9,23 +9,22 @@ if not os.path.exists(specFolder):
     print("Species not found in All category!")
     exit(1)
 if not os.path.exists(outFolder):
-    print(specFolder)
     print("Creating Individual_Seqs folder for " + species + " as " + outFolder)
     os.mkdir(outFolder)
-    with open(os.path.join(specFolder, species, ".txt"), "r") as consensusFile:
+    with open(specFolder + species + ".txt", "r") as consensusFile:
         nucSeqTitle = ""
         nucSeqBuilder = ""
         for line in consensusFile:
             if line.startswith(">"):
                 if nucSeqTitle != "":
-                    with open(os.path.join(outFolder, nucSeqTitle, ".fa"), "w") as outFile:
+                    with open(outFolder + nucSeqTitle + ".fa", "w") as outFile:
                         outFile.write(nucSeqTitle + "\n")
                         outFile.write(nucSeqBuilder)
                 nucSeqTitle = line.strip()
                 nucSeqBuilder = ""
             else:
                 nucSeqBuilder += line.strip().upper()
-        with open(os.path.join(outFolder, nucSeqTitle, ".fa"), "w") as outFile:
+        with open(outFolder + nucSeqTitle + ".fa", "w") as outFile:
             outFile.write(nucSeqTitle + "\n")
             outFile.write(nucSeqBuilder)
 else:
