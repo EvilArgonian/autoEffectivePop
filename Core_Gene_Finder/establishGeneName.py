ef20c3278e93d517a77f180bda04141c36b7f937
EvilArgonian <pmele@carolina.rr.com>
2023-05-22 10:58:22 -0400

    Update establishGeneName.py

--- Core_Gene_Finder/establishGeneName.py
+++ Core_Gene_Finder/establishGeneName.py
@@ -5,13 +5,13 @@ geneFile = sys.argv[1]
 blastFile = sys.argv[2]
 
 with open(blastFile, "r") as b:
-    line = b.readlines[2]
+    line = b.readlines()[2]
     geneBeforeGB = line[line.index(" "): line.index(">")]  # Slices portion including gene name
     try:  # Attempts to remove portion dedicated to species name, if present
         geneBeforeGB = geneBeforeGB[0:geneBeforeGB.index("[")]
     except Exception:
         pass
-    geneName = geneBeforeGB.trim()  # Replace with location of gene name in blast file
-    renaming = geneName.title().replace(" ", "_") + ".txt"
+    geneName = geneBeforeGB.trim().title()  # Replace with location of gene name in blast file
+    renaming = geneName.replace(" ", "_") + ".txt"
 os.rename(geneFile, renaming)
 print(renaming)
