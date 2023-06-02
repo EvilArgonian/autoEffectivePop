7d5fe9b0ed38ada3d6e4a5efc8c0f2df854cd62f
EvilArgonian <pmele@carolina.rr.com>
2023-06-02 12:51:46 -0400

    Tweak Gene Name Finding

--- Core_Gene_Finder/establishGeneName.py
+++ Core_Gene_Finder/establishGeneName.py
@@ -6,11 +6,13 @@ blastFile = sys.argv[2]
 
 with open(blastFile, "r") as b:
     line = b.readlines()[2]
-    geneBeforeGB = line[line.index(" "): line.index(">")]  # Slices portion including gene name
-    try:  # Attempts to remove portion dedicated to species name, if present
-        geneBeforeGB = geneBeforeGB[0:geneBeforeGB.index("[")]
-    except Exception:
-        pass
+    subStrStart = line.index(" ")
+    subStrEnd = line.index("[")
+    geneBeforeGB = line[subStrStart:subStrEnd]  # Slices portion including gene name
+    #try:  # Attempts to remove portion dedicated to species name, if present
+    #    geneBeforeGB = geneBeforeGB[0:geneBeforeGB.index("[")]
+    #except Exception:
+    #    pass
     geneName = geneBeforeGB.trim().title()  # Replace with location of gene name in blast file
     renaming = geneName.replace(" ", "_") + ".txt"
 os.rename(geneFile, renaming)
