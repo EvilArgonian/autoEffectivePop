5c8651d7998089ca5a97dfecd5373a0c039a3bfc
EvilArgonian <pmele@carolina.rr.com>
2023-06-29 00:57:17 -0400

    Create gathering tool for JC output

---
+++ gather_JC_Output.py
@@ -0,0 +1,19 @@
+import os
+import sys
+
+specName = sys.argv[1]
+outFile = sys.argv[2]
+readFile = "final_output/" + specName + "/wattersonsThetaValues.txt"
+
+if os.path.exists(readFile):
+    with open(readFile, "r") as rf:
+        for line in rf.readlines():
+            print("Gathering line: " + line)
+
+            cleanLine = line.replace("_collapsed_mutation_fastas", "")
+            cleanLine = cleanLine.replace(" Watterson's Theta S: ", "")
+            cleanLine = cleanLine.replace(" Watterson's Theta N: ", "")
+            cleanLine = cleanLine.replace(" Watterson's Theta: ", "")
+
+            outFile.write(specName + "\t" + cleanLine + "\n")
+    print("Done with " + specName)
---
+++ gather_JC_Output.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+set -euo pipefail
+IFS=$'\n\t'
+
+echo "Moving JC Modified-Processing Input!"
+
+#Establish what species are being processed; $1 and further arguments should be species names matching those used as directory names in the input folder
+processInput=()
+if [ -z "${1+set}" ]; then 
+    for file in $(find ../../jcaraway/collapsed_fastas/ -mindepth 1 -maxdepth 1 -type d); do
+		processInput+=($file)
+	done
+else 
+	for arg in "$@"; do
+		processInput+=("../../jcaraway/collapsed_fastas/"$arg)
+	done
+fi
+
+echo "Input Processing: ${processInput[@]}"
+
+outTable="JC_Out_Table.txt"
+touch ${outTable}
+
+processCount=0
+for inputFolder in ${processInput[@]}; do
+	{ # Try	
+		inputLabel="${inputFolder##*/}"
+		
+		processCount=$(( ${processCount}+1 ))
+		echo "Gathering output for Species ${processCount}: ${inputLabel}"
+		
+		python gather_JC_Output.py ${inputLabel} ${outTable}
+		
+		if [[ -f "${outFolder}/${inputLabel}/ORF1a_collapsed_mutation_fastas.fa" ]]; then #Check if it contains gene ORF1a, which is contained within ORF1ab, and must be processed separately to avoid counting the sites doubly
+			python gather_JC_Output.py ${inputLabel}_1a ${outTable}
+		fi
+	} || { # Catch
+		echo "Some kind of interrupting error occurred while processing ${inputFolder##*/}"
+	}
+done
