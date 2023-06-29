56c7e8140f6b30961c6ba1ce6f908c27e0e0dcb0
EvilArgonian <pmele@carolina.rr.com>
2023-06-29 01:08:26 -0400

    Update gather_JC_Output.py

--- gather_JC_Output.py
+++ gather_JC_Output.py
@@ -9,9 +9,9 @@ if os.path.exists(readFile):
     with open(readFile, "r") as rf:
         for line in rf.readlines():
             cleanLine = line.replace("_collapsed_mutation_fastas", "")
-            cleanLine = cleanLine.replace(" Watterson's Theta S: ", "")
-            cleanLine = cleanLine.replace(" Watterson's Theta N: ", "")
-            cleanLine = cleanLine.replace(" Watterson's Theta: ", "")
+            cleanLine = cleanLine.replace("Watterson's Theta S: ", "")
+            cleanLine = cleanLine.replace("Watterson's Theta N: ", "")
+            cleanLine = cleanLine.replace("Watterson's Theta: ", "")
 
             with open(outFile, "a") as of:
                 of.write(specName + "\t" + cleanLine)
