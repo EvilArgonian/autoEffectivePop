80c059ab511b1cd95071a8f07184a46535b76c2a
EvilArgonian <pmele@carolina.rr.com>
2023-06-29 01:07:18 -0400

    Update gather_JC_Output.py

--- gather_JC_Output.py
+++ gather_JC_Output.py
@@ -8,8 +8,6 @@ readFile = "final_output/" + specName + "/wattersonsThetaValues.txt"
 if os.path.exists(readFile):
     with open(readFile, "r") as rf:
         for line in rf.readlines():
-            print("Gathering line: " + line)
-
             cleanLine = line.replace("_collapsed_mutation_fastas", "")
             cleanLine = cleanLine.replace(" Watterson's Theta S: ", "")
             cleanLine = cleanLine.replace(" Watterson's Theta N: ", "")
