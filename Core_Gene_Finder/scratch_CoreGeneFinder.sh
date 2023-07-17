3f6a0f9ac9ec86484ec778fac1361b6762a7ef65
EvilArgonian <pmele@carolina.rr.com>
2023-07-17 11:10:34 -0400

    Begin refactor of non-arbitrary gene naming in CoreGeneFinder

--- Core_Gene_Finder/scratch_CoreGeneFinder.sh
+++ Core_Gene_Finder/scratch_CoreGeneFinder.sh
@@ -4,66 +4,110 @@ IFS=$'\n\t'
 
 category=${1} # The name of the category being processed, as it appears in categories directory
 repeatRuns=${2} # The amount of repeat runs to perform, of which the genes that are selected in each of these runs will be considered valid
-matchThreshold=${3} # During BLAST evaluations, the similarity threshold that must be beaten to count as a match and thus keep the gene in the core set. A decimal between 0 and 1, such as '0.9'
-
+matchE_Threshold=${3} # During BLAST evaluations, the e_val threshold that must be beaten (lower is more matching) to count as a match and thus keep the gene in the core set. A decimal between 0 and 1, such as '0.1'
 
 echo "Launching Core Gene Finding for ${category}!"
-
 catSpecies=()
 for specFile in $(find categories/${category} -mindepth 1 -maxdepth 1 -type d); do
-	specFileWithoutFolder="${specFile##*/}"
-	spec="${specFileWithoutFolder%%.txt*}"
-	catSpecies+=(${spec})
-done
-
+		specFileWithoutFolder="${specFile##*/}"
+		spec="${specFileWithoutFolder%%.txt*}"
+		catSpecies+=(${spec})
+	done
 catSize=${#catSpecies[@]}
-randomSize = $((catSize * (9/10))) # 90% of initial size, rounded down
-randomSet = ()
-
-for randomIndex in $(shuf --input-range=0-$(( ${#catSpecies[*]} - 1 )) -n ${randomSize}); do
-	randomSet+=(${catSpecies[${randomIndex}]})
-done
-
+randomSize=$(( ${catSize} * 9 / 10)) # 90% of initial size, rounded down
+echo "${catSize} species in the category; random ${randomSize} in each run."
+echo "Preparing BLAST databases for whole category."
 # Determine what databases already exist (NOTE: All databases are stored in the 'All' category, not other categories, to remove redundancy
 seenDatabases=("Default")
-for spec in ${randomSet[@]}; do
-	if [[ -f category/All/${spec}/${spec}.ndb ]]; then
+for spec in ${catSpecies[@]}; do
+	if [[ -f categories/All/${spec}/${spec}.ndb ]]; then
 		seenDatabases+=("${spec}");
 	fi
 done
 
 # Create any other databases 
-for spec in ${randomSet[@]}; do
+for spec in ${catSpecies[@]}; do
 	if [[ ! ${seenDatabases[@]} =~ "${spec}" ]]; then
 		echo "Making database for ${spec}"
-		../ncbi-blast-2.10.1+/bin/makeblastdb -in categories/${category}/${spec}/${spec}.txt -out categories/All/${spec}/${spec} -dbtype nucl
-		python splitConsensus.py ${spec}
+		../ncbi-blast-2.14.0+/bin/makeblastdb -in categories/${category}/${spec}/${spec}.txt -out categories/All/${spec}/${spec} -dbtype nucl
 	else
 		echo "Database for ${spec} already exists."
 	fi
 done
 
-for indexA in {0..$((randomSize-1))}; do 
-	speciesA=(${randomSet[${indexA}]})
-	outFolder = "categories/${category}/${speciesA}/BLAST/"
-	rm -rf ${outFolder} # Clear prior BLAST output
-	for seqFile in $(find categories/All/${speciesA}/Individual_Seqs -mindepth 1 -maxdepth 1 -type f); do
-		seqFileWithoutFolder="${seqFile##*/}"
-		consensusSeq="${seqFileWithoutFolder%%.txt*}"
-		for indexB in {$((indexA+1))..$((randomSize-1))}; do
-			speciesB=(${randomSet[${indexB}]})
-			databaseB = "categories/All/${speciesB}/${speciesB}"
-			./ncbi-blast-2.10.1+/bin/blastx -task megablast -num_threads 4 -db ${databaseB} -query ${seqFile} -outfmt 6 -num_alignments 1 >> ${outFolder}/${speciesA}_consensusSeq_vs_${speciesB}.txt
-		done
+rm -rf core_genes/${category}/
+mkdir core_genes/${category}/
+for (( runNum=1; runNum<=${repeatRuns}; runNum++ )); do
+	echo "Launching run ${runNum} out of ${repeatRuns}!"
+	mkdir core_genes/${category}/Run_${runNum}
+	
+	randomSet=()
+	indexLimit=$(( ${#catSpecies[@]} - 1 ))
+	for randomIndex in $(shuf -i 0-${indexLimit} -n ${randomSize}); do
+		randomSet+=(${catSpecies[${randomIndex}]})
 	done
-done
-#BLAST results are now in category-specific folders, because although individual BLAST results should be identical at the seq_vs_species level, the amount and combinations of such files varies by category 
+	if [ ${#randomSet[@]} -eq 0 ]; then
+		echo "Random Set acquisition failed!"
+	fi
+	
+	echo ${randomSet[@]} > core_genes/${category}/Run_${runNum}/randomSpecies.txt
 
-# Acquire the initial list of genes from first species, which will be filtered down with each comparison to other species without a sufficient match
-# Note that these genes don't yet have names; they will be labeled
-remainingGenes=()
-for seqFile in $(find categories/All/${randomSet[0]}/Individual_Seqs -mindepth 1 -maxdepth 1 -type f); do
-	seqFileWithoutFolder="${seqFile##*/}"
-	consensusSeq="${seqFileWithoutFolder%%.txt*}"
-	remainingGenes+=${consensusSeq}
+	# Acquire the initial list of genes from first species, which will be filtered down with each comparison to other species without a sufficient match
+	# Note that these genes don't yet have real names; they are labeled arbitrarily
+	python initialGeneSetup.py ${category} ${randomSet[0]} ${runNum}
+	
+	blastOutFolder="core_genes/${category}/Run_${runNum}/BLASTs/"
+	failOutFolder="core_genes/${category}/Run_${runNum}/Fails/"
+	rm -rf ${blastOutFolder}
+	rm -rf ${failOutFolder}
+	mkdir ${blastOutFolder}
+	mkdir ${failOutFolder}
+	
+	remainingGenes=()
+	
+	#Concatenate contents of core_genes/${category}/Run_${runNum}/Genes/ to run against nr database as whole entity
+	renameQuery=core_genes/${category}/Run_${runNum}/massRename.txt
+	blastOutFile=${blastOutFolder}/Rename_${arbitraryGene}.txt
+	
+	cat core_genes/${category}/Run_${runNum}/Genes/Gene_*.txt >> ${renameQuery}
+	../ncbi-blast-2.14.0+/bin/blastx -query ${renameQuery} -db "/home/blastdb/nr" -num_alignments 1 >>  ${blastOutFile}
+	
+	exit # For testing
+	
+	# Modify establishGeneName.py to handle multi-output, place here
+	# X=$(echo $(python establishGeneName.py ${X} ${blastOutFile}))
+	
+	# For each other species...
+	for (( speciesIndex=1; speciesIndex<=$((randomSize-1)); speciesIndex++ )); do
+		species=(${randomSet[${speciesIndex}]})
+		database="categories/All/${species}/${species}"
+		# For each remaining Gene...
+		passedGenes=()
+		for gene in ${remainingGenes[@]}; do
+			geneFile=core_genes/${category}/Run_${runNum}/Genes/${gene}
+			blastOutFile=${blastOutFolder}/${gene}_vs_${species}.txt
+			# echo "BLASTing ${geneFile} against ${species} database"
+			../ncbi-blast-2.14.0+/bin/tblastx -num_threads 4 -db ${database} -query ${geneFile} -outfmt 6 -num_alignments 1 >> ${blastOutFile} 2>/dev/null
+			passFlag=$(echo $(python passGene.py ${geneFile} ${blastOutFile} ${matchE_Threshold}))
+			if [[ ${passFlag}=="Passed!" ]]; then
+				passedGenes+=(${gene})
+			else
+				cp ${geneFile} ${failOutFolder}
+				echo "${gene} ${passFlag}"
+			fi
+		done
+		remainingGenes=${passedGenes}
+		if [ ${#remainingGenes[@]} -eq 0 ]; then
+			break
+		fi
+	done
+	if [ ${#remainingGenes[@]} -eq 0 ]; then
+		echo "No core genes survived!"
+	else
+		echo "${#remainingGenes[@]} survived."
+		printf '%s\n' "${passedGenes[@]}" > core_genes/${category}/Run_${runNum}/Passed_Genes.txt
+	fi
 done
+
+# Run Comparison
+python findSharedCoreGenes.py ${category} core_genes/${category}/Core_Genes.txt
