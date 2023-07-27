bd959732a372d603d1e760c57575dd024c8a940c
EvilArgonian <pmele@carolina.rr.com>
2023-07-27 16:15:44 -0400

    Update setupAgroRhizoInput.sh

--- setupAgroRhizoInput.sh
+++ setupAgroRhizoInput.sh
@@ -4,6 +4,7 @@ IFS=$'\n\t'
 
 specFolder="input/RhizobiumAgrobacterium_Group"
 importFolder="../AgroRhizoGroup"
+unzipFolder="../UnzipAgroRhizoGroup"
 
 echo "Beginning moving of AgroRhizo Input."
 
@@ -11,18 +12,5 @@ for file in $(find ${importFolder} -mindepth 1 -maxdepth 1); do
 	zipLabel="${file##*/}"
 	label="${zipLabel%%.zip*}"
 
-	unzip ${file} -d ${importFolder}/${label}
-	mkdir ${specFolder}/${label}
-	
-	if [[ -f ${label}/ncbi_dataset/data/GCA_*/cds_from_genomic.fna ]]; then
-		codingSeqs=${label}/ncbi_dataset/data/GCA_*/cds_from_genomic.fna
-		cp ${codingSeqs} ${specFolder}/${label}/
-		echo "${label} setup from GCA."
-	elif [[ -f ${label}/ncbi_dataset/data/GCF_*/cds_from_genomic.fna ]]; then
-		codingSeqs=${label}/ncbi_dataset/data/GCA_*/cds_from_genomic.fna
-		cp ${codingSeqs} ${specFolder}/${label}/
-		echo "${label} setup from GCF."
-	fi
-	
+	unzip ${file} -d ${unzipFolder}/${label}
 done
-
