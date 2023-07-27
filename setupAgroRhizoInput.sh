10272f49672b832370342a2d1ff07e5c7dbec33b
EvilArgonian <pmele@carolina.rr.com>
2023-07-27 16:01:17 -0400

    Update setupAgroRhizoInput.sh

--- setupAgroRhizoInput.sh
+++ setupAgroRhizoInput.sh
@@ -16,14 +16,13 @@ for file in $(find ${importFolder} -mindepth 1 -maxdepth 1); do
 	if [[ -f ${label}/ncbi_dataset/data/GCA_*/cds_from_genomic.fna ]]; then
 		codingSeqs=${label}/ncbi_dataset/data/GCA_*/cds_from_genomic.fna
 		echo "${label} is being used from GCA."
-	else if [[ -f ${label}/ncbi_dataset/data/GCF_*/cds_from_genomic.fna ]]; then
+	elif [[ -f ${label}/ncbi_dataset/data/GCF_*/cds_from_genomic.fna ]]; then
 		codingSeqs=${label}/ncbi_dataset/data/GCA_*/cds_from_genomic.fna
 		echo "${label} is being used from GCF."
 	fi
 	
 	mkdir ${specFolder}/${label}
 	cp ${codingSeqs} ${specFolder}/${label}/
-	
 	
 done
 
