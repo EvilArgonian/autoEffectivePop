c608cc73230a0a94b7779691b4105865bda87fb6
EvilArgonian <pmele@carolina.rr.com>
2023-08-14 02:19:18 -0400

    Update initialGeneSetup.py

--- Core_Gene_Finder/initialGeneSetup.py
+++ Core_Gene_Finder/initialGeneSetup.py
@@ -31,79 +31,78 @@ if not os.path.exists(outFolder):
                 if nucSeqTitle != "":
                     nonArbitraryReference = os.path.join("../temp", species, "muscle_output", nucSeqTitle[1:])
                     if os.path.exists(nonArbitraryReference):
-                        if useProtein:
-                            with open(nonArbitraryReference, "r") as refFile:
-                                if useCOGviaProteinID:
-                                    searchTag = "[protein_id="
-                                    searchLen = 12
-                                elif useProtein:
-                                    searchTag = "[protein="
-                                    searchLen = 9
-                                else:
-                                    failCount += 1
-                                    print("No naming implementation enabled.")
-                                    with open(failFile, "a") as fFile:
-                                        fFile.write(nucSeqTitle[1:] + "\t" + str(
-                                            failCount) + "\tNo naming implementation enabled")
-                                    name = "Arbitrary_Gene_" + str(geneNum)
-                                    continue
+                        with open(nonArbitraryReference, "r") as refFile:
+                            if useCOGviaProteinID:
+                                searchTag = "[protein_id="
+                                searchLen = 12
+                            elif useProtein:
+                                searchTag = "[protein="
+                                searchLen = 9
+                            else:
+                                failCount += 1
+                                print("No naming implementation enabled.")
+                                with open(failFile, "a") as fFile:
+                                    fFile.write(nucSeqTitle[1:] + "\t" + str(
+                                        failCount) + "\tNo naming implementation enabled")
+                                name = "Arbitrary_Gene_" + str(geneNum)
+                                continue
 
-                                tagDeterminer = {}  # Records counts of unique tag values for the searched tag.
-                                foundAny = False
-                                for refLine in refFile:
-                                    if refLine.startswith(">") and searchTag in refLine:
-                                        subStrStart = refLine.find(searchTag)
-                                        if subStrStart >= 0:
-                                            subStrEnd = refLine.find("]", subStrStart)
-                                            foundAnyProtID = True
-                                            possTag = refLine[subStrStart + searchLen:subStrEnd]
-                                            if possTag not in list(tagDeterminer.keys()):
-                                                tagDeterminer.update({possTag: 1})
-                                            else:
-                                                tagDeterminer.update({possTag: tagDeterminer.get(possTag) + 1})
-                                if foundAny:
-                                    if len(list(tagDeterminer.keys())) == 1:
-                                        name = list(tagDeterminer.keys())[0].replace(" ", "_").replace("/", "-")
-                                    else:
-                                        if useProtein:  # Clears a list of non-helpful possible protein values from suggestion.
-                                            tagDeterminer.pop("hypothetical protein", 0)
-                                            tagDeterminer.pop("hypothetical_protein", 0)
-                                            tagDeterminer.pop("hypothetical-protein", 0)
-                                            tagDeterminer.pop("unknown protein", 0)
-                                            tagDeterminer.pop("unknown_protein", 0)
-                                            tagDeterminer.pop("unknown-protein", 0)
-                                            tagDeterminer.pop("undefined", 0)
-
-                                        highestCountTag = list(tagDeterminer.keys())[0]
-                                        print("Comparing possible gene names for orthogroup " + nucSeqTitle[1:] + " of species " + str(species) + ":")
-                                        for key in list(tagDeterminer.keys()):
-                                            print("\t" + key + "\t- found in " + str(tagDeterminer.get(key)) + " sequence headers.")
-                                            if tagDeterminer.get(key) > tagDeterminer.get(highestCountTag):
-                                                highestCountTag = key
-                                        if useCOGviaProteinID:
-                                            cogData = csv.reader(open(cogFile, "r"), delimiter=",")
-                                            # Column 3 is ProteinID,  7 is the COG ID, and 11 is the e-value for the protein's match with the COG profile.
-                                            # Above column values are base 1; shifted -1 for programming base 0.
-                                            foundCOG = False
-                                            for row in cogData:
-                                                if row[2] == highestCountTag:
-                                                    name = row[6]
-                                                    foundCOG = True
-                                                    break
-                                            if not foundCOG:
-                                                failCount += 1
-                                                print("No COG group found for protein_id tag in orthogroup " + nucSeqTitle[1:] + " of species " + str(species))
-                                                with open(failFile, "a") as fFile:
-                                                    fFile.write(nucSeqTitle[1:] + "\t" + str(failCount) + "\tNo COG group found for tag: " + highestCountTag)
-                                                name = "Arbitrary_Gene_" + str(geneNum)
-                                        elif useProtein:
-                                            name = highestCountTag.replace(" ", "_").replace("/", "-").replace("/", "-")
+                            tagDeterminer = {}  # Records counts of unique tag values for the searched tag.
+                            foundAny = False
+                            for refLine in refFile:
+                                if refLine.startswith(">") and searchTag in refLine:
+                                    subStrStart = refLine.find(searchTag)
+                                    if subStrStart >= 0:
+                                        subStrEnd = refLine.find("]", subStrStart)
+                                        foundAnyProtID = True
+                                        possTag = refLine[subStrStart + searchLen:subStrEnd]
+                                        if possTag not in list(tagDeterminer.keys()):
+                                            tagDeterminer.update({possTag: 1})
+                                        else:
+                                            tagDeterminer.update({possTag: tagDeterminer.get(possTag) + 1})
+                            if foundAny:
+                                if len(list(tagDeterminer.keys())) == 1:
+                                    name = list(tagDeterminer.keys())[0].replace(" ", "_").replace("/", "-")
                                 else:
-                                    failCount += 1
-                                    print("No " + searchTag + "X] tags identified in orthogroup " + nucSeqTitle[1:] + " of species " + str(species))
-                                    with open(failFile, "a") as fFile:
-                                        fFile.write(nucSeqTitle[1:] + "\t" + str(failCount) + "\tNo " + searchTag + "X] tags")
-                                    name = "Arbitrary_Gene_" + str(geneNum)
+                                    if useProtein:  # Clears a list of non-helpful possible protein values from suggestion.
+                                        tagDeterminer.pop("hypothetical protein", 0)
+                                        tagDeterminer.pop("hypothetical_protein", 0)
+                                        tagDeterminer.pop("hypothetical-protein", 0)
+                                        tagDeterminer.pop("unknown protein", 0)
+                                        tagDeterminer.pop("unknown_protein", 0)
+                                        tagDeterminer.pop("unknown-protein", 0)
+                                        tagDeterminer.pop("undefined", 0)
+
+                                    highestCountTag = list(tagDeterminer.keys())[0]
+                                    print("Comparing possible gene names for orthogroup " + nucSeqTitle[1:] + " of species " + str(species) + ":")
+                                    for key in list(tagDeterminer.keys()):
+                                        print("\t" + key + "\t- found in " + str(tagDeterminer.get(key)) + " sequence headers.")
+                                        if tagDeterminer.get(key) > tagDeterminer.get(highestCountTag):
+                                            highestCountTag = key
+                                    if useCOGviaProteinID:
+                                        cogData = csv.reader(open(cogFile, "r"), delimiter=",")
+                                        # Column 3 is ProteinID,  7 is the COG ID, and 11 is the e-value for the protein's match with the COG profile.
+                                        # Above column values are base 1; shifted -1 for programming base 0.
+                                        foundCOG = False
+                                        for row in cogData:
+                                            if row[2] == highestCountTag:
+                                                name = row[6]
+                                                foundCOG = True
+                                                break
+                                        if not foundCOG:
+                                            failCount += 1
+                                            print("No COG group found for protein_id tag in orthogroup " + nucSeqTitle[1:] + " of species " + str(species))
+                                            with open(failFile, "a") as fFile:
+                                                fFile.write(nucSeqTitle[1:] + "\t" + str(failCount) + "\tNo COG group found for tag: " + highestCountTag)
+                                            name = "Arbitrary_Gene_" + str(geneNum)
+                                    elif useProtein:
+                                        name = highestCountTag.replace(" ", "_").replace("/", "-").replace("/", "-")
+                            else:
+                                failCount += 1
+                                print("No " + searchTag + "X] tags identified in orthogroup " + nucSeqTitle[1:] + " of species " + str(species))
+                                with open(failFile, "a") as fFile:
+                                    fFile.write(nucSeqTitle[1:] + "\t" + str(failCount) + "\tNo " + searchTag + "X] tags")
+                                name = "Arbitrary_Gene_" + str(geneNum)
                     else:
                         failCount += 1
                         print("No muscle output files found for orthogroup " + nucSeqTitle[1:] + " of species " + str(species))
